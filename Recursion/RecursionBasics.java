e2ba641eb4c46690190048f7b901b6fdf723a414
mehsan26 <mehsanfahad@gmail.com>
2023-12-27 17:02:09 +0530

    started learning Recursion

---
+++ Recursion/RecursionBasics.java
@@ -0,0 +1,22 @@
+class RecursionBasics {
+    public static void printDec(int n) {
+        if(n==1) {
+            System.out.print(n);
+            return;
+        }
+        System.out.print(n + " ");
+        printDec(n-1);
+    }
+    public static void printInc(int n) {
+        if(n==1) {
+            System.out.print(n);
+            return;
+        }
+        printInc(n - 1);
+        System.out.print(" "+n);
+    }
+    public static void main(String args[]) {
+        // printDec(10);
+        printInc(10);
+    }
+}
